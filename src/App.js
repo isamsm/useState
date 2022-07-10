92eab2f8a35889aaf88fae5c477da99895c4342c
Isa <isamarsques@gmail.com>
2022-07-10 14:35:07 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import style from './style.css'
 
-function App() {
+const App = () => {
+  const [title, setTitle] = useState(false);
+
+  const [showContent, setShowContent] = useState(false);
+
+  const handleTitle = () => {
+    setTitle(!title);
+  }
+
+  const handleContent = () => {
+    setShowContent(!showContent) //oposto do estado atual
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> {title ? `Hello World` : `Bye Bye World`} </h1>
+      {<ExclusiveContent showContent={showContent} />}
+      {/* passa variavel e  verifica se seu valor é verdadeiro ou falso, se verdadeiro, ela é exibida, se não, não é */}
+      <button onClick={handleTitle}> Mudar titulo </button>
+      &nbsp; 
+      {/* espaço em branco */}
+      <button onClick={handleContent}> {showContent ? `Clique para esconder o conteudo` : `Clique para exibir o conteudo`} </button>
+    </>
   );
+}
+
+const ExclusiveContent = (props) => {
+  return (
+      <div>
+        {props.showContent && <h2> Essa é uma mensagem secreta </h2>}
+      </div>
+  )
 }
 
 export default App;
